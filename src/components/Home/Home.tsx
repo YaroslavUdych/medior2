7add213a5897b0f1fe73c0d5241be443c31ddf79
YaroslavUdych <slayes13@gmail.com>
2025-06-21 02:36:27 +0200

    feat(Home): render MainText component on the homepage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from 'react';
+import MainText from '@/components/MainText/MainText';
 import style from './Home.module.css';
 
 const Home = (): ReactElement => (
   <>
     <h1 className={style.title}>Welcome, medior!</h1>
     <p className={style.paragraph}>Read carefully README.md!</p>
+    <MainText />
   </>
 );
 
