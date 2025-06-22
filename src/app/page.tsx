99845b0eb7068f02b7cdedbdf8466669552d54a3
YaroslavUdych <slayes13@gmail.com>
2025-06-22 05:03:27 +0200

    feat(Page): fetch user list from API and pass to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import { ReactElement } from 'react';
 import { Home } from '@/components';
+import IUser from '@/types/User';
+import publicRuntimeConfig from '@/utils/config';
 
-const Page = (): ReactElement => (
-  <Home />
-);
+const { usersUrl } = publicRuntimeConfig;
+
+const getUsers = async (): Promise<IUser[]> => {
+  try {
+    const res = await fetch(usersUrl);
+    if (!res.ok) {
+      throw new Error('Failed to fetch users');
+    }
+    return await res.json();
+  } catch (error) {
+    throw new Error(`Error fetching users: ${error}`);
+  }
+};
+
+const Page = async (): Promise<ReactElement> => {
+  const users = await getUsers();
+
+  return <Home users={users} />;
+};
 
 export default Page;
