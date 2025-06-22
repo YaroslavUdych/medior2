6e7ac3fa5f58e39c2049899e2f232a14323dd588
YaroslavUdych <slayes13@gmail.com>
2025-06-22 05:00:06 +0200

    feat(Home): integrate UserList component with user props

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,15 @@
 import { ReactElement } from 'react';
 import MainText from '@/components/MainText/MainText';
+import UserList from '@/components/UserList/UserList';
+import IUser from '@/types/User';
 import style from './Home.module.css';
 
-const Home = (): ReactElement => (
+const Home = ({ users }: { users: IUser[] }): ReactElement => (
   <>
     <h1 className={style.title}>Welcome, medior!</h1>
     <p className={style.paragraph}>Read carefully README.md!</p>
     <MainText />
+    <UserList users={users} />
   </>
 );
 
