131514bd59397f363113492b63bea68a546a7147
YaroslavUdych <slayes13@gmail.com>
2025-06-22 04:56:33 +0200

    feat(types): add User type definition

---
+++ src/types/User.ts
@@ -0,0 +1,25 @@
+interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default IUser;
