50a914ec52866d3c32b2e35c246a7d63ab30d0d2
YaroslavUdych <slayes13@gmail.com>
2025-06-22 13:15:39 +0200

    refactor(page): moved fetching function to utils/api.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,6 @@
 import { ReactElement } from 'react';
 import { Home } from '@/components';
-import IUser from '@/types/User';
-import publicRuntimeConfig from '@/utils/config';
-
-const { usersUrl } = publicRuntimeConfig;
-
-const getUsers = async (): Promise<IUser[]> => {
-  try {
-    const res = await fetch(usersUrl);
-    if (!res.ok) {
-      throw new Error('Failed to fetch users');
-    }
-    return await res.json();
-  } catch (error) {
-    throw new Error(`Error fetching users: ${error}`);
-  }
-};
+import { getUsers } from '@/utils/api';
 
 const Page = async (): Promise<ReactElement> => {
   const users = await getUsers();
