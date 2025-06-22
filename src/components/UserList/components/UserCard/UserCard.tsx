bebf41c6d33479ea8652263470ac56c31c6c5d30
YaroslavUdych <slayes13@gmail.com>
2025-06-22 04:51:35 +0200

    feat(UserCard): add UserCard component

---
+++ src/components/UserList/components/UserCard/UserCard.tsx
@@ -0,0 +1,84 @@
+import { ReactElement } from 'react';
+import clsx from 'clsx';
+import IUser from '@/types/User';
+import Link from '@/ui/Link/Link';
+import styles from './UserCard.module.css';
+
+const UserCard = (user: IUser): ReactElement => {
+  const lat = user.address.geo.lat;
+  const lng = user.address.geo.lng;
+
+  const mapUrl = `https://mapy.cz/turisticka?source=coor&id=${lng}%2C${lat}&x=${lng}&y=${lat}&z=16`;
+
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.name}>{user.name}</h2>
+
+      <span className={clsx(styles.subtitle, styles.bigMarginBottom)}>
+        Username
+      </span>
+      <span className={clsx(styles.text, styles.bigMarginBottom)}>
+        {user.username}
+      </span>
+
+      <span className={styles.subtitle}>Email</span>
+      <span className={styles.text}>{user.email}</span>
+
+      <span className={styles.subtitle}>Phone</span>
+      <span className={styles.text}>{user.phone}</span>
+
+      <span className={clsx(styles.subtitle, styles.bigMarginBottom)}>
+        Website
+      </span>
+      <span className={clsx(styles.text, styles.bigMarginBottom)}>
+        {user.website}
+      </span>
+
+      <div className={clsx(styles.group, styles.fullRow)}>
+        <h3 className={styles.title}>Address</h3>
+        <Link href={mapUrl} isExternal>
+          Show on map
+        </Link>
+      </div>
+
+      <span className={styles.subtitle}>Street</span>
+      <span className={styles.text}>{user.address.street}</span>
+
+      <span className={styles.subtitle}>Suite</span>
+      <span className={styles.text}>{user.address.suite}</span>
+
+      <span className={styles.subtitle}>City</span>
+      <span className={styles.text}>{user.address.city}</span>
+
+      <span className={clsx(styles.subtitle, styles.bigMarginBottom)}>
+        Zipcode
+      </span>
+      <span className={clsx(styles.text, styles.bigMarginBottom)}>
+        {user.address.zipcode}
+      </span>
+
+      <h3 className={clsx(styles.title, styles.fullRow)}>Company</h3>
+
+      <span className={styles.subtitle}>Name</span>
+      <span className={styles.text}>{user.company.name}</span>
+
+      <span className={styles.subtitle}>Catch phrase</span>
+      <span className={styles.text}>{user.company.catchPhrase}</span>
+
+      <span className={styles.subtitle}>Bs</span>
+      <span className={styles.text}>{user.company.bs}</span>
+
+      <div className={styles.buttonWrapper}>
+        <Link
+          href={{
+            pathname: `/articles/${user.id}`,
+          }}
+        >
+          Read articles
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
